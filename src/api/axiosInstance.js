3efa681868cc23a27a0975e1390439cc2050693e
jyotish1136 <jyotish1136.gmail.com>
2025-04-14 12:07:22 +0530

    uri update

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://my-notes-backend-qfpu.onrender.com";
 const axiosInstance = axios.create({ baseURL: API_BASE_URL })
 export default axiosInstance;
