df2b37c00e5b5f8c6da96ba33c9bb497c5fdb1c5
jyotish1136 <jyotish1136.gmail.com>
2025-04-14 12:58:03 +0530

    remove strict

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeProvider.jsx";
 import AuthProvider from "./context/AuthContext";
-import UserProvider from "./store/user-provider.jsx";
-import PostListProvider from "./store/post-store-provider.jsx";
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ThemeProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ThemeProvider>
-  </StrictMode>
+  <ThemeProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </ThemeProvider>
 );
